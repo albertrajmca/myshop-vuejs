ebe79f6e92ff058d191430328655fbd8eba09ed5
Albert <albert@Alberts-MacBook-Pro.local>
2023-05-06 14:23:22 +0530

    base vue js set up

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+axios.get('http://localhost:8000/api/products/1/show')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li style="display:block; color:blue; padding:3px;  border:2px" v-for="item in items" :key="item.id">
+                {{ item.name }} || 
+                {{ item.description }} ||
+                {{ item.price }}
+            </li>
+        </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        items: [],
+      }
+    },
+    mounted() {
+     axios.get("http://localhost:8000/api/products/list")
+      .then((response) => {
+        this.items = response.data.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  }
+  </script>
